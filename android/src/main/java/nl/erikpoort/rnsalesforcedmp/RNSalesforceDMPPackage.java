128240815630e1141cc274009e323b38630666f4
Erik Poort <hello@erikpoort.nl>
2018-11-30 17:15:27 +0100

    Add android plugin package

---
+++ android/src/main/java/nl/erikpoort/rnsalesforcedmp/RNSalesforceDMPPackage.java
@@ -0,0 +1,34 @@
+package nl.erikpoort.rnsalesforcedmp;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by erik on 30/11/2018.
+ * Erik Poort 2018
+ */
+
+public class RNSalesforceDMPPackage implements ReactPackage {
+    @Override
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+        List<NativeModule> modules = new ArrayList<>();
+        modules.add(new RNSalesforceDMPPackage(reactContext));
+        return modules;
+    }
+
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+}
