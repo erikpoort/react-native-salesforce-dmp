98e9e61ef7930b39ef3705774c14aae1f23fd65d
Erik Poort <hello@erikpoort.nl>
2018-11-30 16:46:28 +0100

    Remove dry run as Android does not support it

--- src/index.js
+++ src/index.js
@@ -4,16 +4,12 @@ const { RNSalesforceDMPModule } = NativeModules;
 /**
 * Setup tracker
 * @param {string} configId Configuration Id
-* @param {boolean} debugFlag This outputs debug messages while it is
-*     trying to fetch data/send data to Salesforce DMP backend servers.
-*     You should turn this off before the final submission of your app.
-* @param {boolean} dryRunFlag This does a dryRun without sending any
-*     data to the Salesforce DMP servers. This is useful while you are
-*     developing an app. You should turn this off before the final
-*     submission of your app.
+* @param {boolean} debugFlag This outputs debug messages while it is trying to fetch data/send data
+*     to Salesforce DMP backend servers. You should turn this off before the final submission of
+*     your app.
 */
-function setupTracker(configId, debugFlag, dryRunFlag) {
-  RNSalesforceDMPModule.setupTracker(configId, debugFlag, dryRunFlag);
+function setupTracker(configId, debugFlag) {
+  RNSalesforceDMPModule.setupTracker(configId, debugFlag);
 }
 
 /**
