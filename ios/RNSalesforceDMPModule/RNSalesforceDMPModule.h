a63719bdf34e86169150b1d2efe62e0c1353b354
Erik Poort <hello@erikpoort.nl>
2018-11-30 17:32:28 +0100

    Add empty ios module

---
+++ ios/RNSalesforceDMPModule/RNSalesforceDMPModule.h
@@ -0,0 +1,15 @@
+//
+//  RNSalesforceDMPModule.h
+//  RNSalesforceDMPModule
+//
+//  Created by Erik Poort on 30/11/2018.
+//  Copyright (c) 2018 Erik Poort. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <React/RCTBridgeModule.h>
+#import <React/RCTUtils.h>
+
+@interface RNSalesforceDMPModule : NSObject <RCTBridgeModule>
+
+@end
---
+++ ios/RNSalesforceDMPModule/RNSalesforceDMPModule.m
@@ -0,0 +1,14 @@
+//
+//  RNSalesforceDMPModule.m
+//  RNSalesforceDMPModule
+//
+//  Created by Erik Poort on 30/11/2018.
+//  Copyright (c) 2019 MediaMonks. All rights reserved.
+//
+
+#import "RNSalesforceDMPModule.h"
+
+@implementation RNSalesforceDMPModule {
+}
+
+@end
